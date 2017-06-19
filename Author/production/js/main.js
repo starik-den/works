598daada75af6cf2d62750488b219e967e3551c9
starik-den <starik_den@mail.ru>
2017-06-19 11:52:44 +0300

    header, slider, team with styles

---
+++ Author/production/js/main.js
@@ -0,0 +1,32 @@
+//slider1
+;(function ($) {
+
+    $(function () {
+        $('.slider').slick({
+            arrows: true,
+            // autoplay: true,
+            speed: 1500,
+            dots: false
+            // responsive: [
+            //
+            //     {
+            //         breakpoint: 321,
+            //         settings: {
+            //             // slidesToShow: 1,
+            //             // slidesToScroll: 1
+            //             dots: false
+            //
+            //         }
+            //     }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+            // ]
+        });
+
+
+    });
+
+
+})(jQuery);
+
