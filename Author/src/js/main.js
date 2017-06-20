362a81cac9b346e30af8ba24600f0baa280c63ba
starik-den <starik_den@mail.ru>
2017-06-20 13:30:36 +0300

    All without responsive

--- Author/src/js/main.js
+++ Author/src/js/main.js
@@ -7,24 +7,39 @@
             // autoplay: true,
             speed: 1500,
             dots: false
-            // responsive: [
-            //
-            //     {
-            //         breakpoint: 321,
-            //         settings: {
-            //             // slidesToShow: 1,
-            //             // slidesToScroll: 1
-            //             dots: false
-            //
-            //         }
-            //     }
-            // You can unslick at a given breakpoint now by adding:
-            // settings: "unslick"
-            // instead of a settings object
-            // ]
         });
-
-
+        $('.fly').click(function(){
+            var target = $(this).attr('href');
+            $('html, body').animate({scrollTop: $(target).offset().top}, 800);
+            return false;
+        });
+        $('.select__btn').hover(function () {
+            $(this).closest('.select').siblings('.plan__title').toggleClass('active')
+        });
+        $(window).on('scroll', function () {
+            var object = $('.header');
+            if ($(this).scrollTop() > 100) {
+                object.addClass("fixed");
+            } else {
+                object.removeClass("fixed");
+            }
+        });
+        $.validator.addMethod("realname", function(value, element) {
+            return this.optional(element) || /^[а-яё, a-z]+$/i.test(value);
+        }, "Ведите свое имя без цифр");
+        $('#form').validate({
+            rules: {
+                email: {
+                    required: true,
+                    email: true
+                },
+                name: {
+                    required: true,
+                    minlength: 3,
+                    realname: true
+                }
+            }
+        });
     });
 
 
