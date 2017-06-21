94fb81e13e1f4107c6c3fbf280884835b88076c2
starik-den <starik_den@mail.ru>
2017-06-21 12:37:27 +0300

    finish

--- Author/src/js/main.js
+++ Author/src/js/main.js
@@ -1,7 +1,7 @@
 //slider1
 ;(function ($) {
-
     $(function () {
+        //rule for slider
         $('.slider').slick({
             arrows: true,
             // autoplay: true,
@@ -16,14 +16,17 @@
                 }
             ]
         });
+        //rule for fly scroll
         $('.fly').click(function () {
             var target = $(this).attr('href');
             $('html, body').animate({scrollTop: $(target).offset().top}, 800);
             return false;
         });
+        //rule for hover in block "offers"
         $('.select__btn').hover(function () {
             $(this).closest('.select').siblings('.plan__title').toggleClass('active')
         });
+        //rule for fixed menu
         $(window).on('scroll', function () {
             var object = $('.header');
             if ($(this).scrollTop() > 100) {
@@ -32,6 +35,7 @@
                 object.removeClass("fixed");
             }
         });
+        //rule for form validation
         $.validator.addMethod("realname", function (value, element) {
             return this.optional(element) || /^[а-яё, a-z]+$/i.test(value);
         }, "Ведите свое имя без цифр");
@@ -46,6 +50,19 @@
                     minlength: 3,
                     realname: true
                 }
+            }
+        });
+        //rule for responsive menu
+        $('.header__menu-compact').on('click', function () {
+            $(this).next().slideToggle(200)
+        });
+        $(window).on('resize', function () {
+            var menu = $('.header__menu');
+            if ($(window).width() >= 700){
+                menu.show()
+            }
+            else {
+                menu.hide()
             }
         });
     });
